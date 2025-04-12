12a26de7cc51e3e45bbf376f00669479bb9c3148
Ernest Aitiev <valeetmihifaveas@gmail.com>
2025-04-12 17:42:13 +0600

    Got rid of the extra screen widget, added rating editing

--- lib/cinemateque.dart
+++ lib/cinemateque.dart
@@ -1,9 +1,16 @@
 import 'package:cinemateque/models/cinema.dart';
-import 'package:cinemateque/screens/home.dart';
+import 'package:cinemateque/widgets/canvas.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/add.dart';
+import 'screens/details.dart';
+import 'widgets/movie_tile.dart';
+
+
 class Cinemateque extends StatefulWidget {
-  const Cinemateque({super.key});
+  const Cinemateque({
+    super.key,
+  });
 
   @override
   State<Cinemateque> createState() => _CinematequeState();
@@ -15,11 +22,103 @@ class _CinematequeState extends State<Cinemateque> {
   void updateMoviesList(Movie newMovie) {
     setState(() {
       movies.add(newMovie);
+      selectedMovies = getMovies(currentScreenIndex);
     });
+  }
+
+  void updateMovieRating(String movieId, int? rating, bool watched) {
+    setState(() {
+      final movieIdx = movies.indexWhere((movie) => movie.id == movieId);
+      movies[movieIdx].rating = rating;
+      movies[movieIdx].watched = watched;
+      selectedMovies = getMovies(currentScreenIndex);
+    });
+  }
+
+  int currentScreenIndex = 0;
+  late List<Movie> selectedMovies;
+
+  List<Movie> getMovies(int index) {
+    if (currentScreenIndex == 0) {
+      return movies.where((movie) => !movie.watched).toList();
+    } else {
+      return movies.where((movie) => movie.watched).toList();
+    }
+  }
+
+  @override
+  void initState() {
+    selectedMovies = getMovies(currentScreenIndex);
+    super.initState();
+  }
+
+  void updateCurrentPageIndex(int newIndex) {
+    setState(() {
+      currentScreenIndex = newIndex;
+      selectedMovies = getMovies(newIndex);
+    });
+  }
+
+  void showMovieDetails(String id) {
+    var movie = selectedMovies.firstWhere((movie) => movie.id == id);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (ctx) =>
+            MovieDetails(movie: movie, updateRating: updateMovieRating),
+      ),
+    );
+  }
+
+  void openAddMovieSheet() {
+    showModalBottomSheet(
+      context: context,
+      useSafeArea: true,
+      isScrollControlled: true,
+      builder:
+          (ctx) =>
+          Wrap(children: [AddMovieForm(saveMovie: updateMoviesList)]),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Home(movies: movies, onMovieAdded: updateMoviesList);
+    return ScreenCanvas(
+      appBarActions: [
+        IconButton(onPressed: openAddMovieSheet, icon: Icon(Icons.add)),
+      ],
+      bottomBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.movie_creation_outlined),
+            label: "To be seen",
+            selectedIcon: Icon(Icons.movie_creation),
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.star_outline_sharp),
+            label: "Watched",
+            selectedIcon: Icon(Icons.star_outlined),
+          ),
+        ],
+        selectedIndex: currentScreenIndex,
+        onDestinationSelected: updateCurrentPageIndex,
+      ),
+      widgets: [
+        Expanded(
+          child: GridView.builder(
+            itemCount: selectedMovies.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder:
+                (ctx, idx) => MovieTile(
+              movie: selectedMovies[idx],
+              onMovieClicked: showMovieDetails,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -2,30 +2,114 @@ import 'package:cinemateque/models/cinema.dart';
 import 'package:cinemateque/widgets/canvas.dart';
 import 'package:flutter/material.dart';
 
-class MovieDetails extends StatelessWidget {
+class MovieDetails extends StatefulWidget {
   final Movie movie;
+  final void Function(String id, int? rating, bool watched) updateRating;
 
-  const MovieDetails({super.key, required this.movie});
+  const MovieDetails({
+    super.key,
+    required this.movie,
+    required this.updateRating,
+  });
+
+  @override
+  State<MovieDetails> createState() => _MovieDetailsState();
+}
+
+class _MovieDetailsState extends State<MovieDetails> {
+  Set<int> _bookRatingButtonSelection = <int>{};
+
+  void markMovieAsWatched(BuildContext ctx) async {
+    if (!widget.movie.watched) {
+      return showDialog<void>(
+        context: ctx,
+        builder:
+            (ctx) => StatefulBuilder(
+              builder:
+                  (ctx, setState) => AlertDialog(
+                    title: Text("Please, rate the movie"),
+                    content: SegmentedButton<int>(
+                      segments:
+                          [1, 2, 3, 4, 5]
+                              .map<ButtonSegment<int>>(
+                                (value) => ButtonSegment(
+                                  value: value,
+                                  label: Text(value.toString()),
+                                ),
+                              )
+                              .toList(),
+                      selected: _bookRatingButtonSelection,
+                      multiSelectionEnabled: false,
+                      emptySelectionAllowed: true,
+                      onSelectionChanged: (newSelection) {
+                        setState(() {
+                          _bookRatingButtonSelection = newSelection;
+                        });
+                      },
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop();
+                        },
+                        child: Text("cancel"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          if (_bookRatingButtonSelection.isNotEmpty) {
+                            widget.updateRating(
+                              widget.movie.id,
+                              _bookRatingButtonSelection.first,
+                              !widget.movie.watched,
+                            );
+                            Navigator.of(ctx).pop();
+                          }
+                        },
+                        child: Text("Rate"),
+                      ),
+                    ],
+                  ),
+            ),
+      );
+    } else {
+      setState(() {
+        widget.updateRating(
+          widget.movie.id,
+          null,
+          false,
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return ScreenCanvas(
       widgets: [
         Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.network(movie.poster),
-                Divider(thickness: 3.0),
-                Text(
-                  movie.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          child: Column(
+            children: [
+              Image.network(widget.movie.poster),
+              Divider(thickness: 3.0),
+              Text(
+                widget.movie.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              Text(widget.movie.description),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  markMovieAsWatched(context);
+                },
+                child: Text(
+                  widget.movie.watched
+                      ? "Return to 'To be seen'"
+                      : "Mark as watched",
                 ),
-                SizedBox(height: 20),
-                Text(movie.description),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ],
